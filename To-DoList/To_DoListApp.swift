c61f02b78391e8fc4068bd1cac0df076691d6227
Mazen Kapadi <73120392+mazenkapadi@users.noreply.github.com>
2021-12-14 15:00:39 +0530

    Initial Commit

---
+++ To-DoList/To_DoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoListApp.swift
+//  To-DoList
+//
+//  Created by Mazen Kapadi on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct To_DoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
